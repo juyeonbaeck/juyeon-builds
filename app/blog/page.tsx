a9c2965fdddaf731637d6ec914979e093eb445af
juyeon <juyeon.it@gmail.com>
2025-05-04 13:45:59 +0900

    Initial Commit - 0504

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,11 +1,11 @@
 import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
-import { genPageMetadata } from 'app/seo'
+import { genPageMetadata } from 'app/seo.js'
 import ListLayout from '@/layouts/ListLayoutWithTags'
 
 const POSTS_PER_PAGE = 5
 
-export const metadata = genPageMetadata({ title: 'Blog' })
+export const metadata = genPageMetadata({title: 'blog', description: '', image: '' })
 
 export default async function BlogPage(props: { searchParams: Promise<{ page: string }> }) {
   const posts = allCoreContent(sortPosts(allBlogs))
